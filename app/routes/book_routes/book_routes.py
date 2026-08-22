55cd1edc6015582917700ec255068036c20cc116
Supreme_Engineer98 <gmavropouloscoding@gmail.com>
2026-08-22 21:55:42 +0300

    Feature:get all books endpoint

--- app/routes/book_routes/book_routes.py
+++ app/routes/book_routes/book_routes.py
@@ -20,9 +20,19 @@ async def create_book(book:BookCreate,book_service= Depends(get_book_service)):
     #call the create method from book service
     new_book = await book_service.create_book(book)
     #return the result
-    return new_book
+    return {"message":"The book created with success","data":new_book}
 
 
+
+#get books endpoint
+@router.get('/',status_code=status.HTTP_200_OK)
+#method to get all books
+async def get_books(skip:int = 0,limit:int = 100,book_service= Depends(get_book_service)):
+    #call get all books method
+    books = await book_service.get_all_books(skip,limit)
+
+    #return the books 
+    return {"message":"Success","data":books}
 
 
 
