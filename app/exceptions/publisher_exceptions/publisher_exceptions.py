729bd41bc548b12bc879c214abb938ae098b6df1
Supreme_Engineer98 <gmavropouloscoding@gmail.com>
2026-08-23 18:11:25 +0300

    Feat: refactor publisher exception to use the exception handler class

--- app/exceptions/publisher_exceptions/publisher_exceptions.py
+++ app/exceptions/publisher_exceptions/publisher_exceptions.py
@@ -3,8 +3,10 @@
 from ..base_exception_class import ExceptionServiceHandler
 
 class DuplicatePublisher(ExceptionServiceHandler):
-    pass
+    def __init__(self,message: str ="There is another publisher registered with the same name"):
+                super().__init__(status_code=409,message=message, code="Duplicate publisher")
 
 
 class PublisherNotFound(ExceptionServiceHandler):
-    pass
+    def __init__(self,message: str ="There publisher you are looking for does not exist"):
+                super().__init__(status_code=409,message=message, code="Publisher not found")
